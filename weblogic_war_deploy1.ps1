903906ea361cc6cffdab2157f7fdb3a6a59a18ce
MullaAfiya <135946533+MullaAfiya@users.noreply.github.com>
2024-10-02 22:32:23 +0530

    Update weblogic_war_deploy1.ps1

--- weblogic_war_deploy1.ps1
+++ weblogic_war_deploy1.ps1
@@ -1,1 +1,22 @@
+﻿
+# Set the variables for the deployement
+#USE t3 INSTEAD OF http IF YOU HAVE NOT ENABLED THE TUNNELING. IF YOU WANT TO ENABLE TUNNELING IN SERVER>PROTOCALS>GENERAL>ENABLE TUNNELING
+$adminUrl = "http://desktop-jh9jp1c:7001/console"
+$username = "weblogic"
+$password = "weblogic123"
+$appPath =  "C:/DevOpsSoftware/weblogic/devopstool.war"
+$target = "AdminServer"
 
+# Deploy the application using weblogic.Deployer
+#"YOU CAN FIND WEBLOGIC.JAR IN GIVEN PATH:- Oracle_Home\wlserver\server\lib\weblogic.jar (WEBLOGIC.DEPLOYER IS PRESENT IN THE WEBLOGIC.JAR AS A CLASSPATH(CP))"
+java -cp "C:/Oracle/Middleware/Oracle_Home/wlserver/server/lib/weblogic.jar" weblogic.Deployer `
+ -verbose `
+ -noexit `
+ -adminurl $adminUrl `
+ -username $username `
+ -password $password `
+ -deploy $appPath `
+ -targets $target `
+ -name devopstool
+
+Write-Host "Application Deployed successfully."
